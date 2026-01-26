23e7bb3b8160ed52dae65002f2685cc386665207
diolino <gtdiolino@gmail.com>
2026-01-26 20:06:07 -0300

    retirando api de info conteiner

--- api/routes/tarefas.js
+++ api/routes/tarefas.js
@@ -1,29 +1,7 @@
-const os = require('os'); 
-const fs = require('fs');
-
 module.exports = (app) => {
   const controllerFactory = require("../controllers/tarefas");
   const controller = controllerFactory();
-
-  app.route("/api/info") 
-    .get((req, res) => { 
-      
-      try { const cgroup = fs.readFileSync("/proc/self/cgroup", "utf8"); 
-        const hostname = os.hostname(); 
-        let containerId = "unknown"; 
-        containerId = cgroup.split("\n")[0].split("/").pop(); 
-        const instanceId = Math.random().toString(36).substring(7); 
-        res.json({ hostname, containerId, instanceId }); 
-      } catch (err) {
-        res.status(500).send({ 
-          message: "Erro ao obter informações do contêiner." ,
-          error: err.message, // mensagem simples stack: err.stack // opcional: detalhes do stack trace
-          stack: err.stack // opcional: detalhes do stack trace
-        });
-      } 
-     
-    });
-
+  
   app.route("/api/tarefas")
     .get(async (req, res, next) => {
       try {
