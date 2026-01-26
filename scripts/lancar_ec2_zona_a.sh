319da7ef1425c82fc18ddb1ecf625db75a2e312f
diolino <gtdiolino@gmail.com>
2026-01-26 15:13:22 -0300

    rodar

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -1,5 +1,5 @@
-vpc_id=$(aws ec2 describe-vpcs --filters Name=isDefault,Values=true --query "Vpcs[0].VpcId" --output text)
-subnet_id=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=$vpc_id Name=availabilityZone,Values=us-east-1a --query "Subnets[0].SubnetId" --output text)
+vpc_id=vpc-029718ff06294e6ee
+subnet_id=subnet-0c9b103ed894e8e53
 security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
 
 if [ -z "$security_group_id" ]; then
@@ -7,7 +7,7 @@ if [ -z "$security_group_id" ]; then
     exit 1
 fi
 
-aws ec2 run-instances --image-id ami-02f3f602d23f1659d --count 1 --instance-type t3.micro \
+aws ec2 run-instances --image-id ami-0f85876b1aff99dde --count 1 --instance-type t3.micro \
 --security-group-ids $security_group_id --subnet-id $subnet_id --associate-public-ip-address \
 --block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":15,"VolumeType":"gp2"}}]' \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bia-dev}]' \
