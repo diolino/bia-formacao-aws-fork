f286e35cc5901324df1fafb075d0ad50f10e6bc1
diolino <gtdiolino@gmail.com>
2026-03-03 22:01:05 -0300

    script lancar ec2

--- scripts/lancar_ec2_zona_a.sh
+++ scripts/lancar_ec2_zona_a.sh
@@ -1,13 +1,18 @@
-vpc_id=vpc-029718ff06294e6ee
-subnet_id=subnet-0c9b103ed894e8e53
-security_group_id=$(aws ec2 describe-security-groups --group-names "bia-dev" --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
+#Parâmetros para criação da instância EC2
+vpc_id=$1
+#vpc-0f615c3491c1ec286
+subnet_id=$2
+#subnet-0d67fed54556307a5
+image_id=$3
+#ami-0f3caa1cf4417e51b
+security_group_id=$( aws ec2 describe-security-groups --filters Name=vpc-id,Values=$vpc_id Name=group-name,Values=bia-dev --query "SecurityGroups[0].GroupId" --output text 2>/dev/null)
 
 if [ -z "$security_group_id" ]; then
     echo ">[ERRO] Security group bia-dev não foi criado na VPC $vpc_id"
     exit 1
 fi
 
-aws ec2 run-instances --image-id ami-0f85876b1aff99dde --count 1 --instance-type t3.micro \
+aws ec2 run-instances --image-id $image_id --count 1 --instance-type t3.micro \
 --security-group-ids $security_group_id --subnet-id $subnet_id --associate-public-ip-address \
 --block-device-mappings '[{"DeviceName":"/dev/xvda","Ebs":{"VolumeSize":15,"VolumeType":"gp2"}}]' \
 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=bia-dev}]' \
